20bbf611b238dc3639ff2c739adfccd52f424e7a
ohjonah <ohjonah@gmail.com>
2017-05-22 15:05:54 -0700

    added constructor

---
+++ app.js
@@ -0,0 +1,29 @@
+'use strict';
+
+function Images(name, path, shown, clicked) {
+  this.name = name;
+  this.path = path;
+  this.shown = false;
+  this.clicked = 0;
+}
+
+var bag = new Images('bag', 'img/' + name + '.jpg', false, 0);
+var banana = new Images('banana', 'img/' + name + '.jpg', false, 0);
+var bathroom = new Images('bathroom', 'img/' + name + '.jpg', false, 0);
+var boots = new Images('boots', 'img/' + name + '.jpg', false, 0);
+var breakfast = new Images('breakfast', 'img/' + name + '.jpg', false, 0);
+var bubblegum = new Images('bubblegum', 'img/' + name + '.jpg', false, 0);
+var chair = new Images('chair', 'img/' + name + '.jpg', false, 0);
+var cthulhu = new Images('cthulhu', 'img/' + name + '.jpg', false, 0);
+var dogDuck = new Images('dog-duck', 'img/' + name + '.jpg', false, 0);
+var dragon = new Images('dragon', 'img/' + name + '.jpg', false, 0);
+var pen = new Images('pen', 'img/' + name + '.jpg', false, 0);
+var petSweep = new Images('pet-sweep', 'img/' + name + '.jpg', false, 0);
+var scissors = new Images('scissors', 'img/' + name + '.jpg', false, 0);
+var shark = new Images('shark', 'img/' + name + '.jpg', false, 0);
+var sweep = new Images('sweep', 'img/' + name + '.png', false, 0);
+var tauntaun = new Images('tauntaun', 'img/' + name + '.jpg', false, 0);
+var unicorn = new Images('unicorn', 'img/' + name + '.jpg', false, 0);
+var usb = new Images('usb', 'img/' + name + '.gif', false, 0);
+var waterCan = new Images('water-can', 'img/' + name + '.jpg', false, 0);
+var wineGlass = new Images('wine-glass', 'img/' + name + '.jpg', false, 0);
