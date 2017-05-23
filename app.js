0ae84af741633d5fffdc54c0dd276261011b1e3a
ohjonah <ohjonah@gmail.com>
2017-05-23 06:55:52 -0700

    added clicked image aggregator

--- app.js
+++ app.js
@@ -15,24 +15,24 @@ var breakfast = new Images('breakfast', 'img/breakfast.jpg', false, 0);
 var bubblegum = new Images('bubblegum', 'img/bubblegum.jpg', false, 0);
 var chair = new Images('chair', 'img/chair.jpg', false, 0);
 var cthulhu = new Images('cthulhu', 'img/cthulhu.jpg', false, 0);
-var dogDuck = new Images('dog-duck', 'img/dog-duck.jpg', false, 0);
+var dogduck = new Images('dog-duck', 'img/dog-duck.jpg', false, 0);
 var dragon = new Images('dragon', 'img/dragon.jpg', false, 0);
 var pen = new Images('pen', 'img/pen.jpg', false, 0);
-var petSweep = new Images('pet-sweep', 'img/pet-sweep.jpg', false, 0);
+var petsweep = new Images('pet-sweep', 'img/pet-sweep.jpg', false, 0);
 var scissors = new Images('scissors', 'img/scissors.jpg', false, 0);
 var shark = new Images('shark', 'img/shark.jpg', false, 0);
 var sweep = new Images('sweep', 'img/sweep.jpg', false, 0);
 var tauntaun = new Images('tauntaun', 'img/tauntaun.jpg', false, 0);
 var unicorn = new Images('unicorn', 'img/unicorn.jpg', false, 0);
 var usb = new Images('usb', 'img/usb.gif', false, 0);
-var waterCan = new Images('water-can', 'img/water-can.jpg', false, 0);
-var wineGlass = new Images('wine-glass', 'img/wine-glass.jpg', false, 0);
+var watercan = new Images('water-can', 'img/water-can.jpg', false, 0);
+var wineglass = new Images('wine-glass', 'img/wine-glass.jpg', false, 0);
 
 var counter = 0;
 var randNumSetOne = [];
-var randNumSetTwo = [];
+var chosenImages = [];
 
-var gallery = [bag, banana, bathroom, boots, breakfast, bubblegum, chair, cthulhu, dogDuck, dragon, pen, petSweep, scissors, shark, sweep, tauntaun, unicorn, usb, waterCan, wineGlass];
+var gallery = [bag, banana, bathroom, boots, breakfast, bubblegum, chair, cthulhu, dogduck, dragon, pen, petsweep, scissors, shark, sweep, tauntaun, unicorn, usb, watercan, wineglass];
 
 var imageOne = document.getElementById('image-one');
 var imageTwo = document.getElementById('image-two');
@@ -75,19 +75,23 @@ function randomImgOnDom() {
 function clickImage() {
   imageOne.addEventListener('click', function() {
     var srcAttr = this.getAttribute('src');
-    srcAttr.split('.jpg')[0].split('/')[1].clicked++;
-    console.log(srcAttr.split('.jpg')[0].split('/')[1]);
-    console.log(srcAttr.split('.jpg')[0].split('/')[1].clicked);
+    chosenImages.push(srcAttr.split('.jpg')[0].split('/')[1]);
+    console.log(chosenImages);
+    counter++;
   });
 
   imageTwo.addEventListener('click', function() {
     var srcAttr = this.getAttribute('src');
-    srcAttr.split('.jpg')[0].split('/')[1].clicked++;
+    chosenImages.push(srcAttr.split('.jpg')[0].split('/')[1]);
+    console.log(chosenImages);
+    counter++;
   });
 
   imageThree.addEventListener('click', function() {
     var srcAttr = this.getAttribute('src');
-    srcAttr.split('.jpg')[0].split('/')[1].clicked++;
+    chosenImages.push(srcAttr.split('.jpg')[0].split('/')[1]);
+    console.log(chosenImages);
+    counter++;
   });
 }
 
@@ -97,6 +101,16 @@ randomImgOnDom();
 clickImage();
 secondRandNumGenerator();
 console.log('rand two', randNumSetOne);
+secondRandNumGenerator();
+console.log('rand three', randNumSetOne);
+secondRandNumGenerator();
+console.log('rand four', randNumSetOne);
+secondRandNumGenerator();
+console.log('rand five', randNumSetOne);
+secondRandNumGenerator();
+console.log('rand six', randNumSetOne);
+secondRandNumGenerator();
+console.log('rand seven', randNumSetOne);
 
   // imageOne.setAttribute('src', gallery[]);
 
