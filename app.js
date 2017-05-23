f405e2dda0a0b4ff70fc72230e7a4686bad83126
ohjonah <ohjonah@gmail.com>
2017-05-23 13:41:56 -0700

    added image index converter for cliked images

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 'use strict';
 
-var counter = 0;
+var counter = 1;
 var randNumSet = [];
-var chosenImages = [];
+var clickedImages = [];
 var shownImages =[];
 
 var allImages = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dogduck', 'dragon', 'pen', 'petsweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'watercan', 'wineglass'];
@@ -12,6 +12,8 @@ var allImageObjects = [];
 function Images(name, path) {
   this.name = name;
   this.path = path;
+  this.clicked = 0;
+  this.shown = 0;
 }
 
 function instantiateImages() {
@@ -27,27 +29,6 @@ function instantiateImages() {
     }
   }
 }
-
-// var bag = new Images('bag', 'img/bag.jpg');
-// var banana = new Images('banana', 'img/banana.jpg');
-// var bathroom = new Images('bathroom', 'img/bathroom.jpg');
-// var boots = new Images('boots', 'img/boots.jpg');
-// var breakfast = new Images('breakfast', 'img/breakfast.jpg');
-// var bubblegum = new Images('bubblegum', 'img/bubblegum.jpg');
-// var chair = new Images('chair', 'img/chair.jpg');
-// var cthulhu = new Images('cthulhu', 'img/cthulhu.jpg');
-// var dogduck = new Images('dogduck', 'img/dog-duck.jpg');
-// var dragon = new Images('dragon', 'img/dragon.jpg');
-// var pen = new Images('pen', 'img/pen.jpg');
-// var petsweep = new Images('petsweep', 'img/pet-sweep.jpg');
-// var scissors = new Images('scissors', 'img/scissors.jpg');
-// var shark = new Images('shark', 'img/shark.jpg');
-// var sweep = new Images('sweep', 'img/sweep.png');
-// var tauntaun = new Images('tauntaun', 'img/tauntaun.jpg');
-// var unicorn = new Images('unicorn', 'img/unicorn.jpg');
-// var usb = new Images('usb', 'img/usb.gif');
-// var watercan = new Images('watercan', 'img/water-can.jpg');
-// var wineglass = new Images('wineglass', 'img/wine-glass.jpg');
 
 var imageOne = document.getElementById('image-one');
 var imageTwo = document.getElementById('image-two');
@@ -84,42 +65,51 @@ function randomImgOnDom() {
 function clickImage() {
   imageOne.onclick = function() {
     var srcAttr = this.getAttribute('src');
-    chosenImages.push(srcAttr.split('.')[0].split('/')[1]);
+    clickedImages.push(srcAttr.split('.')[0].split('/')[1]);
     refreshedImages();
   };
 
   imageTwo.onclick = function() {
     var srcAttr = this.getAttribute('src');
-    chosenImages.push(srcAttr.split('.')[0].split('/')[1]);
+    clickedImages.push(srcAttr.split('.')[0].split('/')[1]);
     refreshedImages();
   };
 
   imageThree.onclick = function() {
     var srcAttr = this.getAttribute('src');
-    chosenImages.push(srcAttr.split('.')[0].split('/')[1]);
+    clickedImages.push(srcAttr.split('.')[0].split('/')[1]);
     refreshedImages();
   };
-  console.log('chosen images arr:', chosenImages);
-  console.log('chosen images length:',chosenImages.length);
+  console.log('clicked images arr:', clickedImages);
+  console.log('clicked images length:',clickedImages.length);
 }
 
 function refreshedImages() {
 
   if (counter === 25) {
     alert('done');
+    clickedImageIndexConverter();
 
   } else {
     randNumGenerator();
     randomImgOnDom();
     clickImage();
     counter++;
-    console.log('outside of scope:', counter);
-    console.log('shown images', shownImages);
+    console.log('shown images:', shownImages);
   }
 }
 
+function clickedImageIndexConverter() {
+  for (var i = 0; i < clickedImages.length; i++) {
+    clickedImages[i] = allImages.indexOf(clickedImages[i]);
+  }
+}
+
+function clickedImageCalc() {
+  
+}
+
 instantiateImages();
-console.log(allImageObjects);
 randNumGenerator();
 randomImgOnDom();
 clickImage();
